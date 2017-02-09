2fd0db3f6800bd8efd2d1f938dfb7021d0586c4e
Iain Reid <iainreid382@gmail.com>
2017-02-09 14:38:15 +0000

    Added missing utils method

--- source/lib/utilities/utilities.ts
+++ source/lib/utilities/utilities.ts
@@ -13,3 +13,16 @@ export function firstDefinedProperty(props: string[]): (obj: Object) => any {
     return R.prop(R.compose(R.head, R.curry(R.intersection)(props), R.keys)(obj), obj);
   };
 }
+
+/**
+ * Push a single element to the array provided and ensure that scope of the 'push' is correct.
+ *
+ * @param {Any[]} arr - The array to push to
+ *
+ * @returns {Function} A method that will push the provided value to the array
+ */
+export function pushToArray(arr: any[]): (value: any) => number {
+  return (value: any): number  => {
+    return arr.push(value);
+  };
+}
