b3e43f899dc9a10961ae41b7865fa912052c030f
Iain Reid <iainreid382@gmail.com>
2017-02-10 12:19:53 +0000

    Added new methods with documentation

--- source/lib/utilities/utilities.ts
+++ source/lib/utilities/utilities.ts
@@ -22,7 +22,75 @@ export function firstDefinedProperty(props: string[]): (obj: Object) => any {
  * @returns {Function} A method that will push the provided value to the array
  */
 export function pushToArray(arr: any[]): (value: any) => number {
-  return (value: any): number  => {
+  return (value: any): number => {
     return arr.push(value);
   };
 }
+
+/**
+ * Ensure that the getters and setters over the target object are case insensitive.
+ *
+ * @param {Object} target - The target object
+ *
+ * @return {Proxy} A proxy over the target object ensuring that the getters and setters are case insensitive
+ */
+export function makeCaseInsensitive<T>(target: T): T {
+  return new Proxy<T>(target, {
+    get: caseInsensitivePropGet,
+    set: caseInsensitivePropSet,
+    getOwnPropertyDescriptor: caseInsensitivePropDescriptor
+  });
+}
+
+/**
+ * Perform a case insensitive lookup on the target object.
+ *
+ * @private
+ *
+ * @param {Object} target - The target object
+ * @param {String} prop   - The target property
+ *
+ * @returns {Any} Any value found at the lower cased property lookup
+ */
+function caseInsensitivePropGet(target: { [x: string]: any }, prop: string): any {
+  return target[prop.toLowerCase()];
+}
+
+/**
+ * Perform a case insensitive property set on the target object.
+ *
+ * @private
+ *
+ * @param {Object} target - The target object
+ * @param {String} prop   - The target property
+ * @param {Any}    value  - The value to set
+ *
+ * @returns {Boolean} A success value after setting the property
+ */
+function caseInsensitivePropSet(target: { [x: string]: any }, prop: string, value: any): boolean {
+  target[prop.toLowerCase()] = value;
+
+  return true;
+}
+
+/**
+ * Perform a case insensitive property descriptor lookup on the target object.
+ *
+ * @private
+ *
+ * @param {Object} target - The target object
+ * @param {String} prop   - The target property
+ *
+ * @returns {Boolean} A success value after setting the property
+ */
+function caseInsensitivePropDescriptor<T>(target: T, prop: string): Object | undefined {
+  const value = target.hasOwnProperty(prop.toLowerCase());
+
+  return value ? {
+    value,
+    writable: true,
+    enumerable: true,
+    configurable: true
+  } : undefined;
+}
+
